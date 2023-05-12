202595d07dedf838cf5cfa86203dd415d92aea0f
lee4339 <skyho4339@naver.com>
2023-05-12 23:28:56 +0900

    개인프로젝트 시작 전 셋팅 중 1

---
+++ src/main/java/com/ghproject/ghprojectno0120230510/GhprojectNo0120230510Application.java
@@ -0,0 +1,13 @@
+package com.ghproject.ghprojectno0120230510;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhprojectNo0120230510Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhprojectNo0120230510Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghproject/ghprojectno0120230510/GhprojectNo0120230510ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghproject.ghprojectno0120230510;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhprojectNo0120230510ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
