22bb35e0d7d414bc897ee3cb89fbe8fe1d20a4ac
lee4339 <skyho4339@naver.com>
2023-05-22 14:43:37 +0900

    개인프로젝트 시작 전 셋팅 2

---
+++ src/main/java/com/ghproject/ghprojectno0120230510/controller/PageController.java
@@ -0,0 +1,9 @@
+package com.ghproject.ghprojectno0120230510.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class PageController {
+}
